d324a6da3a5af509ad08fe305a6bd800a1bd5bef
vitluchko <vitluchko@gmail.com>
2024-07-08 20:20:47 +0300

    added confernce seeder

--- optima/database/seeders/DatabaseSeeder.php
+++ optima/database/seeders/DatabaseSeeder.php
@@ -23,5 +23,6 @@ class DatabaseSeeder extends Seeder
 
         $this->call(AdminRoleSeeder::class);
         $this->call(AdminUserSeeder::class);
+        $this->call(ConferenceSeeder::class);
     }
 }
